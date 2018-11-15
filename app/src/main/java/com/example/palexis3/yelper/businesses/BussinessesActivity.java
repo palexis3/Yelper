eeb2763e3702f3e78b9fb6b34941ae813e1e0b9c
palexis3 <alexispatrick77@gmail.com>
2018-11-15 15:45:24 -0800

    Added necessary UI layouts for representing models

--- app/src/main/java/com/example/palexis3/yelper/businesses/BussinessesActivity.java
+++ app/src/main/java/com/example/palexis3/yelper/businesses/BussinessesActivity.java
@@ -14,7 +14,7 @@ public class BussinessesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bussiness);
+        setContentView(R.layout.activity_business);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
